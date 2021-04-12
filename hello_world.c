3d36f6744cfe25d2d7f1a2bee877b971b63e63f8
vjp3008 <81668061+vjp3008@users.noreply.github.com>
2021-04-13 00:18:26 +0530

    Create hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,12 @@
+*******************************************************************************/
+// B.Tech Cyber Security 20C23012 - Patel Vraj Jignesh
+#include <stdio.h>
+
+int
+main ()
+{
+  printf ("Hello World\n");
+  printf ("Program executed by 20C23012 Vraj Jignesh Patel");
+
+  return 0;
+}
